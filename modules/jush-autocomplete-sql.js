a1c44b289f90c11e2b83022ffccb9765147720e6
Peter Knut <peter@pematon.com>
2025-08-28 14:25:28 +0200

    Fix offering FROM while wring a new query at the beginning (fix #24)

--- modules/jush-autocomplete-sql.js
+++ modules/jush-autocomplete-sql.js
@@ -48,6 +48,8 @@ jush.autocompleteSql = function (esc, tablesColumns) {
 			.replace(/^\s+/, '')
 		;
 		after = after.replace(/;[^]*/, ''); // strip next query
+		// strip subquery or a next query if edited query is not terminated yet
+		after = after.replace(/\b(CREATE|ALTER|RENAME|DROP|TRUNCATE|SELECT|INSERT|UPDATE|DELETE|USE|DELIMITER|EXPLAIN)(\s.*|$)/s, '');
 		const query = before + after;
 		const allTables = Object.keys(tablesColumns);
 		const usedTables = findTables(query); // tables used by the current query
